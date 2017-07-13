3f88bceed80801e7472670eba074c2cea6aead3b
zhangzhengyan <zzy_19831106@163.com>
2017-07-13 14:35:59 +0800

    v4.0
    实现功能规格2.0的功能
    添加了主机集群管理功能，cpu、mem、disk和net card的监控功能

--- nsm_thread.py
+++ nsm_thread.py
@@ -17,6 +17,9 @@ import os
 import sys
 from nsm_lib import get_path_capacity
 from nsm_lib import fs_root_dir
+import re
+import subprocess
+import psutil
 
 #集群节点系统信息配置文件目录
 sys_conf = "/var/nsm_agent/myself"
@@ -95,18 +98,15 @@ class send_thread(threading.Thread):
             # print "10 second run"
             #send heartbeat every 10 second
             # send_heartbeat(self.channel, self.exchange, self.routing_key)
-<<<<<<< HEAD
             # send_cifs_conn(self.channel, self.exchange, self.routing_key)
             # send_nfs_conn(self.channel, self.exchange, self.routing_key)
-=======
-            send_cifs_conn(self.channel, self.exchange, self.routing_key)
->>>>>>> b8e498420a53de900d0a07694df4ce309b004d39
+            send_node_info(self.channel, self.exchange, self.routing_key)
             end_time = time.time()
 
             return int((end_time - start_time) / sleep_interval)
         if run_interval == self.task_run_interval["30_SEC_TASK"]:
             # print "30 second run"
-            send_fs_capacity(self.channel, self.exchange, self.routing_key)
+            # send_fs_capacity(self.channel, self.exchange, self.routing_key)
             end_time = time.time()
 
             return int((end_time - start_time) / sleep_interval)
@@ -117,7 +117,7 @@ class send_thread(threading.Thread):
             return int((end_time - start_time) / sleep_interval)
         if run_interval == self.task_run_interval["300_SEC_TASK"]:
             #update register every 300 second
-            send_register(self.channel, self.exchange, self.routing_key)
+            # send_register(self.channel, self.exchange, self.routing_key)
             end_time = time.time()
 
             return int((end_time - start_time) / sleep_interval)
@@ -223,7 +223,6 @@ def send_heartbeat(channel, exchange, routing_key):
     channel.basic_publish(exchange=exchange, routing_key=routing_key, body=message)
 
 def send_cifs_conn(channel, exchange, routing_key):
-<<<<<<< HEAD
 
     message = {}
 
@@ -231,16 +230,6 @@ def send_cifs_conn(channel, exchange, routing_key):
     message["id"] = 0
     #消息的类型
     message["type"] = nsm_agent_modules.message_type["NSM_AGENT_CHECK"]
-=======
-    message = {}
-
-    #注册消息id为0
-    message["id"] = 0
-    #注册消息的类型
-    message["type"] = nsm_agent_modules.message_type["NSM_AGENT_CHECK"]
-    #注册类型其实只有这一个消息，功能id应该为0，但是这里将节点的状态一并发送，功能id打上状态id
-    #由于注册消息定时发送，所有这样没问题，要是注册只注册一次那么状态消息应该独立出来
->>>>>>> b8e498420a53de900d0a07694df4ce309b004d39
     message["fun_id"] = nsm_agent_modules.check_fun_id["NSM_AGENT_CHECK_CIFS_CONN"]
     message["echo"] = False
     message["sync"] = False
@@ -302,7 +291,6 @@ def send_cifs_conn(channel, exchange, routing_key):
 
     #------------------------------------------------------------------------------------
     message = json.dumps(message, ensure_ascii=False)
-<<<<<<< HEAD
     # print message
 
     channel.basic_publish(exchange=exchange, routing_key=routing_key, body=message)
@@ -397,11 +385,237 @@ def send_fs_capacity(channel, exchange, routing_key):
     else:
         message["body"]["free_space"] = avail
 
+# 发送节点的硬件信息，包括cpu、网卡等
+def send_node_info(channel, exchange, routing_key):
+    import socket
+    message = {}
+
+    # 消息id为0
+    message["id"] = 0
+    # 消息的类型
+    message["type"] = nsm_agent_modules.message_type["NSM_AGENT_CHECK"]
+    # func id
+    message["fun_id"] = nsm_agent_modules.check_fun_id["NSM_AGENT_CHECK_NODE_INFO"]
+    message["echo"] = False
+    message["sync"] = False
+    message["timestamp"] = time.time()
+    message["shost"] = nsm_lib.getLocalIp()
+
+    # fill body
+    message["body"] = {}
+
+    # get node、cpu 、mem 、disk info， use psutil module fun
+
+    # get cpu info -----------------------------------------------------------------------------
+    message["body"]["cpu"] = {}
+    # try:
+    #     cmd = "top -bn1 | grep \"Cpu(s)\" | sed \"s/.*, *\\([0-9.]*\\)%* id.*/\\1/\" | awk '{print 100-$1}'"
+    #     result = os.popen(cmd, 'r').readline()
+    #     message["body"]["cpu"]["cpu_usage"] = result
+    # except:
+    #     pass
+
+    try:
+        message["body"]["cpu"]["cpu_usage"] = psutil.cpu_percent(1)
+    except:
+        pass
+
+    #get mem usage---------------------------------------------------------------------------
+    try:
+        mem = psutil.virtual_memory()
+        message["body"]["cpu"]["mem_usage"] = str(mem.percent)
+    except:
+        pass
+
+    #get net info-----------------------------------------------------------------------------
+    message["body"]["net_card"] = []
+    net_card_list = message["body"]["net_card"]
+    net_card = {}
+
+    stats = psutil.net_if_stats()
+    for nic, addrs in psutil.net_if_addrs().items():
+        net_card = {}
+        if nic == "lo":
+            continue
+        else:
+            net_card["name"] = nic
+        if nic in stats:
+            st = stats[nic]
+            if st.isup:
+                net_card["status"] = 1
+            else:
+                net_card["status"] = 0
+        for addr in addrs:
+            if addr.family == socket.AF_INET or addr.family == socket.AF_INET6:
+                net_card["ip"] = addr.address
+            elif addr.family == psutil.AF_LINK:
+                net_card["mac"] = addr.address
+        net_card_list.append(net_card)
+
+    # #get net card info
+    # message["body"]["net_card"] = []
+    # res_list = message["body"]["net_card"]
+    #
+    # #get name mac ip status等
+    # ifc = subprocess.Popen(["ifconfig", "-a"], stdout=subprocess.PIPE)
+    # res = {}
+    # ip = None
+    # mac = None
+    # ip_status = 0
+    # mac_status = 0
+    #
+    # for x in ifc.stdout:
+    #     if not x.strip():
+    #         # 空行
+    #         if not res: #如果res是空后面不用处理
+    #             continue
+    #         if ip:
+    #             res["ip"] = ip.group(1)
+    #         else:
+    #             res["ip"] = ""
+    #
+    #         if mac:
+    #             res["mac"] = mac.group(1)
+    #         else:
+    #             res["mac"] = ""
+    #         res_list.append(res)
+    #
+    #         res = {}
+    #         ip = None
+    #         mac = None
+    #         ip_status = 0
+    #         mac_status = 0
+    #     elif not res:
+    #         # 第一行
+    #         try:
+    #             name = re.match(r'[A-Za-z0-9-]+', x).group()
+    #             if name == "lo":
+    #                 # lo 后面的行都会走这个流程
+    #                 continue
+    #             res["name"] = name
+    #             if re.search(r'UP', x):
+    #                 res["status"] = 1
+    #             else:
+    #                 res["status"] = 0
+    #         except:
+    #             #这个地方后续要处理一下
+    #             pass
+    #     else:
+    #         # 其他行
+    #         try:
+    #             if ip_status == 0 and not ip:
+    #                 ip = re.match(r'\s+inet\s+(\S+)', x)
+    #             if ip:
+    #                 ip_status = 1
+    #             if mac_status == 0 and not mac:
+    #                 mac = re.match(r'\s+ether\s+(\S+)', x)
+    #             if mac:
+    #                 mac_status = 1
+    #         except:
+    #             # 这个地方后续要处理一下
+    #             pass
+
+    #得到每个网卡的收发速率
+    for i in range(len(net_card_list)):
+        name = net_card_list[i]["name"]
+        cmd = "iftop -i " + name + " -t -s 1 | grep -i \"^total\" | awk -F\":\" '{print $2}' | awk '{print $1}'"
+        j = 0
+        try:
+            for line in os.popen(cmd).readlines():
+                if j == 0:
+                    net_card_list[i]["send_rate"] = str(line)
+                elif j == 1:
+                    net_card_list[i]["rec_rate"] = str(line)
+                j += 1
+        except:
+            pass
+
+    #得到磁盘信息--------------------------------------------------------------
+    message["body"]["disk"] = []
+    disk_table = message["body"]["disk"]
+    disk = {}
+
+    #先使用shell命令得到磁盘列表
+    # cmd = "lsblk | awk '{if($1~/^.{1}d[a-z]/)print $1}'"
+    cmd = "lsblk | awk '{if($1~/^[a-z].*/)print $1}'"
+    disk_list = []
+    for line in os.popen(cmd).readlines():
+        line = line.strip('\n')
+        disk_list.append(line)
+
+    for part in psutil.disk_partitions(all=False):
+        if os.name == 'nt':
+            if 'cdrom' in part.opts or part.fstype == '':
+                # skip cd-rom drives with no disk in it; they may raise
+                # ENOENT, pop-up a Windows GUI error for a non-ready
+                # partition or just hang.
+                continue
+
+        # 挂载点没有ceph盘的标识
+        if not re.search("ceph", part.mountpoint):
+            #已经挂载的但是非ceph使用的磁盘从磁盘列表移除
+            for i in range(len(disk_list)):
+                if re.search(disk_list[i], part.device):
+                    disk_list.pop(i)
+                    break
+            continue
+
+
+        '''
+        disk dic example:
+        disk =  {
+                    "sda":{"size":314151235, "used":4534535},
+                    "sdb":{"size":314151235, "used":4534535},
+                    "sdc":{"size":314151235, "used":4534535}
+                }
+        '''
+        for i in range(len(disk_list)):
+            if re.search(disk_list[i], part.device):
+                if not disk.has_key(disk_list[i]):
+                    disk[disk_list[i]] = {}
+                usage = psutil.disk_usage(part.mountpoint)
+
+                if not disk[disk_list[i]].has_key("size"):
+                    disk[disk_list[i]]["size"] = int(usage.total)
+                else:
+                    disk[disk_list[i]]["size"] += int(usage.total)
+
+                if not disk[disk_list[i]].has_key("used"):
+                    disk[disk_list[i]]["used"] = int(usage.used)
+                else:
+                    disk[disk_list[i]]["used"] += int(usage.used)
+
+    for key in disk.keys():
+        disk_info = {}
+        disk_info["name"] = key
+        disk_info["size"] = disk[key]["size"]
+        disk_info["used"] = disk[key]["used"]
+        disk_info["status"] = 1
+
+        disk_table.append(disk_info)
+
+
+    #现在cpeh使用的磁盘已经记录在disk字典里了，我们从disk_list里删除disk字典记录的磁盘
+    for key in disk.keys():
+        disk_list.remove(key)
+
+    print disk_list
+
+
+    #现在如果disk_list还有元素，说明是未使用的磁盘, 这个用命令取在psutil模块中不能取到没有挂载的块设备
+    for disk_name in disk_list:
+        cmd = "lsblk -b | grep " + disk_name + " | awk '{print $4}'"
+        result = os.popen(cmd).readline()
+        result.strip("\n")
+        disk_info = {}
+        disk_info["name"] = disk_name
+        disk_info["size"] = int(result)
+        disk_info["used"] = 0
+        disk_info["status"] = 0
+        disk_table.append(disk_info)
 
     #------------------------------------------------------------------------------------
     message = json.dumps(message, ensure_ascii=False)
-=======
->>>>>>> b8e498420a53de900d0a07694df4ce309b004d39
     print message
 
     channel.basic_publish(exchange=exchange, routing_key=routing_key, body=message)
